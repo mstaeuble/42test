0c9816f211ea8220107e34493e14241cde5e9ae9
Markus Stäuble <staeuble@gmail.com>
2021-08-30 18:59:44 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(200)
+})
